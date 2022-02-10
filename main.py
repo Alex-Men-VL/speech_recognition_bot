3f4eeaa01ddfe2395fcaaea07f806ff0d6821854
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-02-10 20:59:12 +0300

    Setup DialogFlow

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ env.read_env()
 
 def start_bot():
     tg_token = env.str('TELEGRAM_BOT_TOKEN')
+    project_id = env.str('PROJECT_ID')
+    language_code = env.str('LANGUAGE_CODE', 'ru')
     bot = TgBot(
-        tg_token=tg_token
+        tg_token=tg_token,
+        project_id=project_id,
+        language_code=language_code
     )
     bot.updater.start_polling()
     bot.updater.idle()
--- tg_bot.py
+++ tg_bot.py
@@ -1,31 +1,51 @@
+from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
-    CallbackContext,
 )
 
 
 class TgBot:
 
-    def __init__(self, tg_token):
+    def __init__(self, tg_token, project_id, language_code):
         self.tg_token = tg_token
+        self.project_id = project_id
+        self.language_code = language_code
         self.updater = Updater(token=tg_token, use_context=True)
         self.updater.dispatcher.add_handler(
-            MessageHandler(Filters.text & ~Filters.command, handle_message)
+            MessageHandler(Filters.text & ~Filters.command, self.handle_message)
         )
         self.updater.dispatcher.add_handler(
             CommandHandler('start', handle_start_message)
         )
 
+    def handle_message(self, update: Update, _):
+        chat_id = update.message.chat_id
+        received_message = update.message.text
+
+        message = detect_intent_texts(self.project_id, chat_id, received_message, self.language_code)
+        update.message.reply_text(message)
+
 
 def handle_start_message(update: Update, _):
     user = update.effective_user
-    update.message.reply_text(f'Привет, {user.first_name}.')
+    update.message.reply_text(f'Привет, {user.first_name}!')
 
 
-def handle_message(update: Update, _):
-    user_message = update.message.text
-    update.message.reply_text(user_message)
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={
+            "session": session,
+            "query_input": query_input
+        }
+    )
+    return response.query_result.fulfillment_text
