22de880bf36cdf8be4a7221313446e3c69a1b7f6
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-02-11 14:51:50 +0300

    Add tg logs handler

--- start_tg_bot.py
+++ start_tg_bot.py
@@ -2,10 +2,11 @@ import logging
 
 from config import (
     tg_token,
+    tg_dev_chat_id,
     project_id,
     language_code,
 )
-from tg_bot import TgBot
+from tg_bot import TgBot, TelegramLogsHandler
 
 logger = logging.getLogger('tg_bot')
 
@@ -16,8 +17,11 @@ def start_tg_bot():
         project_id=project_id,
         language_code=language_code
     )
-    logger.info('The tg bot is running')
-    bot.updater.start_polling(drop_pending_updates=True)
+    logs_handler = TelegramLogsHandler(bot.updater.bot, tg_dev_chat_id)
+    logger.addHandler(logs_handler)
+    logger.info('Telegram bot is running')
+
+    bot.updater.start_polling()
     bot.updater.idle()
 
 
--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,25 @@
+import logging
+
 from google.cloud import dialogflow
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
 )
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 class TgBot:
