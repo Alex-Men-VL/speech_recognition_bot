9492f14481051121e490775ec6a791729f5eaa2f
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-02-11 14:39:43 +0300

    Rename main file

--- start_tg_bot.py
+++ start_tg_bot.py
@@ -3,28 +3,28 @@ import logging
 from config import (
     tg_token,
     project_id,
-    language_code
+    language_code,
 )
 from tg_bot import TgBot
 
 logger = logging.getLogger('tg_bot')
 
 
-def start_bot():
+def start_tg_bot():
     bot = TgBot(
         tg_token=tg_token,
         project_id=project_id,
         language_code=language_code
     )
-    logger.info('The bot is running')
-    bot.updater.start_polling()
+    logger.info('The tg bot is running')
+    bot.updater.start_polling(drop_pending_updates=True)
     bot.updater.idle()
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     try:
-        start_bot()
+        start_tg_bot()
     except Exception as err:
         logger.error(err)
 
